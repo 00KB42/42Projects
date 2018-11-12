2fb3dec6cc357eea1a638860f67fad3df32b8dbb
Kyle Brown <00KB42@gmail.com>
2018-11-12 14:01:29 -0800

    norme2 +header file inclusion

--- ft_strncat.c
+++ ft_strncat.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 {
 	size_t	src_ctr;
