f10713bdd36b7ced9e7f027a21f4edc28c675646
00KB42 <00KB42@gmail.com>
2018-11-28 15:11:37 -0800

    ok

--- libft_2.0/ft_strmap.c
+++ libft_2.0/ft_strmap.c
@@ -3,15 +3,11 @@
 char * ft_strmap(char const *s, char (*f)(char))
 {
     int i;
-    
+    char *ptr;
+    for (i = 0; s[i]; i++);
+    ptr = (char*)malloc(sizeof(char) * (i + 1));
+    for (i = 0; s[i]; i++)
+        ptr[i] = (f(s[i]));
+    ptr[i] = '\0';
+    return (ptr);
 }
-
-/*
-
-
-
-
-
-
-
-*/
--- libft_2.0/ft_strmapi.c
+++ libft_2.0/ft_strmapi.c
@@ -1,1 +1,13 @@
 #include "libft.h"
+
+char * ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    int i;
+    char *ptr;
+    for (i = 0; s[i]; i++);
+    ptr = (char*)malloc(sizeof(char) * (i + 1));
+    for (i = 0; s[i]; i++)
+        ptr[i] = (f(s[i]));
+    ptr[i] = '\0';
+    return (ptr);
+}
