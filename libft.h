48d5b395ab6ac364a69353081ee7b082c98e6977
00KB42 <00KB42@gmail.com>
2018-11-28 16:35:51 -0800

    ok

--- libft.h
+++ libft.h
@@ -15,6 +15,8 @@
 # include <stdlib.h>
 # include <unistd.h> // Verify w/ PDF
 
+// Part 1 Funtions
+
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
@@ -45,6 +47,44 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 
-void    ft_putchar_fd(char c, int fd); // Section 2
+// Part 2 Functions
+
+/*
+void * ft_memalloc(size_t size);
+void ft_memdel(void **ap);
+char * ft_strnew(size_t size);
+void ft_strdel(char **as);
+void ft_strclr(char *s);
+void ft_striter(char *s, void (*f)(char *));
+void ft_striteri(char *s, void (*f)(unsigned int, char *));
+char * ft_strmap(char const *s, char (*f)(char));
+char * ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int ft_strequ(char const *s1, char const *s2);
+int ft_strnequ(char const *s1, char const *s2, size_t n);
+char * ft_strsub(char const *s, unsigned int start, size_t len);
+char * ft_strjoin(char const *s1, char const *s2);
+char * ft_strtrim(char const *s);
+char ** ft_strsplit(char const *s, char c);
+char * ft_itoa(int n);
+void ft_putchar(char c);
+void ft_putstr(char const *s);
+void ft_putendl(char const *s);
+void ft_putnbr(int n);
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char const *s, int fd);
+void ft_putendl_fd(char const *s, int fd);
+void ft_putnbr_fd(int n, int fd);
+*/
+
+// Bonus Functions
+
+/*
+t_list * ft_lstnew(void const *content, size_t content_size);
+void ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void ft_lstadd(t_list **alst, t_list *new);
+void ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list * ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem));
+*/
 
 #endif
