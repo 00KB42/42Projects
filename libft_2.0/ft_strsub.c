fe4a88ee2798da12c40005e041a89716e2c8a819
00KB42 <00KB42@gmail.com>
2018-11-29 19:08:27 -0800

    ok

--- libft_2.0/ft_strsub.c
+++ libft_2.0/ft_strsub.c
@@ -2,10 +2,21 @@
 
 char * ft_strsub(char const *s, unsigned int start, size_t len)
 {
-    const char *ptr;
-    ptr = s;
- 
-    int d = ((int)len + (int)start);
-    ptr = (char *)malloc(10 * sizeof(char));
-    return ((char*)&ptr[d]);
+    char *ptr;
+    size_t i;
+    
+    i = 0;
+    ptr = (char*)malloc(len * sizeof(char));
+    if (ptr)
+    {
+        while (s[start] && i < len)
+        {
+            ptr[i] = s[start];
+            start++;
+            i++;
+        }
+        return(ptr);
+    }
+    else
+        return (NULL);
 }
--- libft_2.0/ft_strtrim.c
+++ libft_2.0/ft_strtrim.c
@@ -2,9 +2,43 @@
 
 char * ft_strtrim(char const *s)
 {
-    const char *ptr;
-    ptr = s;
-    ptr = (char*)malloc(sizeof(char));
-    return ((char*)ptr);
+    char *ptr;
+    int i;
+    int forward_i;
+    int backward_i;
+    
+    i = 0;
+    forward_i = 0;
+    backward_i = 0;
+    while (s[backward_i])
+        backward_i++;
+    backward_i--; // otherwise on NULL terminator
+    if (s[forward_i] == ' ' || s[forward_i] == '\n' || s[forward_i] == '\t')
+    {
+        while (s[forward_i] == ' ' || s[forward_i] == '\n' || s[forward_i] == '\t')
+            forward_i++;
+    }
+    else
+        return ((char *)s);
+    if (s[backward_i] == ' ' || s[backward_i] == '\n' || s[backward_i] == '\t')
+    {
+        while (s[backward_i] == ' ' || s[backward_i] == '\n' || s[backward_i] == '\t')
+            backward_i--;
+    }
+    else
+        return ((char *)s);
+    ptr = (char*)malloc(((backward_i + 2) - forward_i) * sizeof(char));
+    if (ptr)
+    {
+        while (forward_i < (backward_i + 1))
+        {
+            ptr[i] = s[forward_i];
+            forward_i++;
+            i++;
+        }
+        ptr[i] = '\0';
+        return (ptr);
+    }
+    else
+        return (NULL);
 }
-
