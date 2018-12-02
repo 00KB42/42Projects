bf7562b03d48a77f563d357dbeb9530b6d8df1bf
00KB42 <00KB42@gmail.com>
2018-12-01 22:55:38 -0800

    ok

--- libft_2.0/ft_strsplit.c
+++ libft_2.0/ft_strsplit.c
@@ -1,5 +1,7 @@
 #include "libft.h"
 
+
+
 char ** ft_strsplit(char const *s, char c)
 {
     int i;
@@ -8,6 +10,8 @@ char ** ft_strsplit(char const *s, char c)
     int word_cnt;
     int ctr_1; // first of word segment
     int ctr_2; // end of word segment
+    int y; // array iterator
+    int x; // array iterator
     char **array;
     
     i = 0;
@@ -16,21 +20,23 @@ char ** ft_strsplit(char const *s, char c)
     word_cnt = 0;
     ctr_1 = 0;
     ctr_2 = 0;
+    x = 0;
+    y = 0;
     
     while (s[i]) // number of strings and longest str_len
     {
-        i = 0;
         if (s[i] == c)
         {
             i++;
         }
         else //new string creation
         {
-            ctr_1 = s[i];
-            while (!(s[i] == c))
+            ctr_1 = i;
+            ctr_2 = i;
+            while (!(s[ctr_2] == c) && s[ctr_2])
             {
                 ctr_2++;
-                i++;
+                i++; //to keep track of location in original string
             }
             if (word_len < (ctr_2 - ctr_1))
                 word_len = (ctr_2 - ctr_1);
@@ -38,29 +44,31 @@ char ** ft_strsplit(char const *s, char c)
         }
     }
     i = 0;
-    array = (char**)malloc(str_ct * sizeof(char**));
-    for(i=0;i<10;i++)
-        array[i]=(char *)malloc(word_len * sizeof(char));
+    array = (char**)malloc(str_ct * sizeof(char*));
+    for(i=0;i<str_ct;i++)
+        array[i]=(char *)malloc((word_len + 1) * sizeof(char));
     i = 0;
     while (s[i]) // number of strings and longest str_len
     {
-        i = 0;
         if (s[i] == c)
         {
             i++;
         }
         else //new string creation
         {
-            ctr_1 = s[i];
+            ctr_1 = i;
+            ctr_2 = i;
             x = 0; // reset str to index 0
-            while (!(s[i] == c))
+            while (!(s[i] == c) && s[i])
             {
                 array[y][x] = s[i];
                 ctr_2++;
                 i++;
                 x++; //same row and string
             }
+            array[y][x] = '\0';
             y++; // moving down one row
         }
     }
+    return(array);
 }
