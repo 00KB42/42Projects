7bfa16a385e7567cebad53dd8eb11f62ca401bbb
00KB42 <00KB42@gmail.com>
2018-12-26 22:56:18 -0800

    ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,42 +14,24 @@
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size)
 {
-	size_t		dst_len;
-	size_t		src_to_cpy;
-	size_t		nbr_cpyd;
-	size_t		src_len;
-	size_t		dst_orig;
-	size_t		i;
+	size_t	src_ctr;
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	dst_len_orig;
 
-	src_len = 0;
-	i = 0;
-	dst_len = 0;
-	src_to_cpy = 0;
-	nbr_cpyd = 0;
-	dst_orig = 0;
-	
-	src_len = ft_strlen(src);
+	src_ctr = 0;
 	dst_len = ft_strlen(dst);
-	dst_orig = dst_len;
+	src_len = ft_strlen(src);
+	dst_len_orig = dst_len;
 	if (size < dst_len)
-		dst_orig = size;
-	src_to_cpy = (size - dst_len - 1);
-	if ((int)src_to_cpy < 0)
+		return (src_len + size);
+	while (src[src_ctr] && dst_len < (size - 1))
 	{
-		nbr_cpyd = src_len + dst_orig; // if size is less than len of dst, add len of src and dst_totrav
-		return (nbr_cpyd);
+		dst[dst_len] = src[src_ctr];
+		dst_len++;
+		src_ctr++;
 	}
-	if (src[i] && i < src_to_cpy)
-	{
-		while (src[i] && i < src_to_cpy)
-		{
-			dst[dst_len] = src[i];
-			dst_len++;
-			i++;
-		}
-		dst[dst_len] = '\0';
-	}
-	nbr_cpyd = src_len + dst_orig; // if size not goofy, you're getting length of src you coied plus length of dst copied..
-	return (nbr_cpyd);
+	dst[dst_len] = '\0';
+	return ((src_len + dst_len_orig));
 }
 
