05dce3213a59d0b7217af179f98fddca0ef0f186
00KB42 <00KB42@gmail.com>
2018-12-27 00:17:56 -0800

    ok

--- libft_2.0/ft_strsplit.c
+++ libft_2.0/ft_strsplit.c
@@ -3,47 +3,44 @@
 char	**ft_strsplit(char const *s, char c)
 {
 	int i;
+	int j;
 	int str_ct;
 	int word_len;
-	int ctr_1; // first of word segment
-	int ctr_2; // end of word segment
 	int y; // array iterator
 	int x; // array iterator
 	char **array;
 	
 	i = 0;
+	j = 0;
 	str_ct = 0;
 	word_len = 0;
-	ctr_1 = 0;
-	ctr_2 = 0;
 	x = 0;
 	y = 0;
-	while (s[i]) // number of strings and longest str_len
-	{
+	while (s[i]) //srt_ctr function
 		if (s[i] == c)
 			i++;
-		else //new string creation
+		else 
 		{
-			ctr_1 = i;
-			ctr_2 = i;
-			while (s[i] != c && s[i])
-			{
-				ctr_2++;
-				i++; //to keep track of location in original string
-			}
-			if (word_len < (ctr_2 - ctr_1))
-				word_len = (ctr_2 - ctr_1);
 			str_ct++;
-			i++;
+			while (s[i] && s[i] != c)
+				i++;
 		}
-	}
 	array = (char**)malloc((str_ct + 1) * sizeof(char*));
 	i = 0;
-	while(i <= str_ct)
-	{
-		array[i]=(char *)malloc((word_len + 1) * sizeof(char));
-		i++;
-	}
+	while (s[j]) //srt_ctr function
+		if (s[j] == c)
+			j++;
+		else 
+		{
+			while (s[j] && s[j] != c)
+			{
+				word_len++;
+				j++;
+			}
+			array[i]=(char *)malloc((word_len + 1) * sizeof(char));
+			i++;
+			word_len = 0;
+		}
 	i = 0;
 	while (s[i]) // number of strings and longest str_len
 	{
