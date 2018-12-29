9bc9961540aa7011e55086499b07bd620dc40f0e
00KB42 <00KB42@gmail.com>
2018-12-29 11:22:08 -0800

    ok

--- libft_2.0/ft_strsplit.c
+++ libft_2.0/ft_strsplit.c
@@ -1,55 +1,82 @@
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+int ft_wrd_ctr (char const *s, char c)
 {
-	int i;
-	int j;
-	int str_chr_ctr;
-	int y; // array iterator
-	int x; // array iterator
-	char **array;
-	
-	i = 0;
-	j = 0;
-	str_chr_ctr = 0;
-	x = 0;
-	y = 0;
-	while (s[i]) //srt_ctr function
-		if (s[i] == c)
-			i++;
-		else 
-		{
-			str_chr_ctr++;
-			while (s[i] && s[i] != c)
-				i++;
-		}
-	array = (char**)malloc((str_chr_ctr + 1) * sizeof(char*));
-	i = 0;
-	str_chr_ctr = 0;
-	while (s[j])
-		if (s[j] == c)
-			j++;
-		else 
-		{
-			while (s[j] && s[j] != c)
-			{
-				str_chr_ctr++;
-				j++;
-			}
-			array[i]=(char *)malloc((str_chr_ctr + 1) * sizeof(char));
-			i++;
-			while (str_chr_ctr > 0)
-			{
-				array[y][x] = s[j - str_chr_ctr];
-				x++; //same row and string
-				str_chr_ctr--;
-			}
-			str_chr_ctr = 0;
-			array[y][x] = '\0';
-			x = 0;
-			y++; // moving down one row
-		}
-	array[y] = NULL;
-	return(array);
+    int i;
+    int wrd_cnt;
+
+    i = 0;
+    wrd_cnt = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            i++;
+        else 
+        {
+            wrd_cnt++;
+            while (s[i] && s[i] != c)
+                i++;
+        }
+    }
+    return (wrd_cnt);
+}
+
+char *ft_fill_arr (const char *s, char c)
+{
+    int i;
+    int chr_ctr;
+    int x;
+    int y;
+    char *ptr;
+
+    i = 0;
+    chr_ctr = 0;
+    x = 0;
+    y = 0;
+    while (s[i] && s[i] != c)
+    {
+        chr_ctr++;
+        i++;        
+    }
+    ptr = (char *)malloc((chr_ctr + 1) * sizeof(char));
+    while (chr_ctr > 0)
+    {
+        ptr[x] = s[i - chr_ctr];
+        x++; //same row and string
+        chr_ctr--;
+    }
+    ptr[x] = '\0';
+    return (ptr);
+}
+
+char    **ft_strsplit(char const *s, char c)
+{
+    int i;
+    int j;
+    int wrd_cnt;
+    char **array;
+    char **ptr;
+
+    i = 0;
+    j = 0;
+    wrd_cnt = 0;
+    wrd_cnt = ft_wrd_ctr(s, c);
+    array = (char**)malloc((wrd_cnt + 1) * sizeof(char*));
+    while (s[i])
+    {
+        if (s[i] == c)
+            i++;
+        else
+        { 
+            s = &s[i];
+            ptr = &array[j];
+            array[j] = ft_fill_arr(s, c);
+            j++;
+            while (s[i] && s[i] != c)
+                i++;
+        }
+    }
+    array[j] = NULL;
+    return(array);
 }
 
