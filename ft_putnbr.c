fca925e7f1b8c062976566863e1e43d623070743
00KB42 <00KB42@gmail.com>
2019-03-07 00:08:23 -0800

    final

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -14,31 +14,22 @@
 
 void	ft_putnbr(int n)
 {
-	int i;
-
-	i = 0;
-	if (n == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = n * -1;
-	}
-	while (n > 10)
-	{
-		ft_putnbr(n / 10);
-		n = (n % 10) + '0';
-		write(1, &n, 1);
-		return ;
-	}
-	n = (n % 10) + '0';
-	write(1, &n, 1);
+    int i;
+    
+    i = 0;
+    if (n == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+        return ;
+    }
+    if (n < 0 && n > -2147483648)
+    {
+        write(1, "-", 1);
+        n = n * -1;
+    }
+    if (n > 9 && n > -2147483648)
+        ft_putnbr(n / 10);
+    n = (n % 10) + '0';
+    write(1, &n, 1);
+    return ;
 }
-/*
-**recursively divides until n is < 10
-**subsequent recursions, take remainder (last #)
-**first recursion, take remainder (last #)
-*/
