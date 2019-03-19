7f877d4c04324c45881aee45867c8979b6a11f39
00KB42 <00KB42@gmail.com>
2019-03-19 12:18:28 -0700

    final

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -14,7 +14,9 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-    while (lst)
+    if (!lst || !f)
+        return ;
+    while (lst != NULL)
     {
         f(lst);
         lst = lst->next;
