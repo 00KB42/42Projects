a6d22b2dfb3c7c3472d9896b88590994e2c33c0d
00KB42 <00KB42@gmail.com>
2019-03-18 19:19:01 -0700

    ok

--- libft.h
+++ libft.h
@@ -21,11 +21,11 @@ typedef struct		s_list
 	size_t			content_size;
 	struct s_list	*next;
 }					t_list;
+size_t				ft_strlen(const char *s);
 void				*ft_memset(void *b, int c, size_t len);
 char				*ft_strdup(const char *s1);
 char				*ft_strchr(const char *s, int c);
 int					ft_strcmp(const char *s1, const char *s2);
-size_t				ft_strlen(const char *s);
 char				*ft_strcpy(char *dst, const char *src);
 char				*ft_strncpy(char *dst, const char *src, size_t len);
 int					ft_isdigit(int c);
